731240af4f5bc88739fa1790bba6115712e79ffc
Run <41304785+Runnnnnnn@users.noreply.github.com>
2023-11-09 19:18:13 -0500

    Update Sample.R

--- Sample.R
+++ Sample.R
@@ -12,14 +12,14 @@ interval = 10
 
 sample_store = matrix(runif(n,0,1),sample_size,n)
 
-average_sum = rowMeans(sample_store)
+average_table = rowMeans(sample_store)
 
 c = rep(0,sample_size/interval)
 j=0
 
 for (i in seq(1,sample_size,interval)){
   j=j+1
-  c[j] = sum(average_sum[i:i+interval-1])
+  c[j] = sum(average_table[i:i+interval-1])
   
 }
 
