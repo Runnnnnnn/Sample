8516ceb6522413ccf831ba95a3540a9d74253a02
Run <41304785+Runnnnnnn@users.noreply.github.com>
2023-11-09 19:52:09 -0500

    Update CLT.R

--- CLT.R
+++ CLT.R
@@ -6,6 +6,7 @@ sample_store = matrix(runif(n*sample_size,0,1),ncol = n)
 
 means = apply(sample_store,2,mean)
 sds = apply(sample_store,2,sd)
-means = (means-1/2)/(sds/sqrt(n))
+means = (means-1/2)/(sds/sqrt(sample_size))
+
 
 hist(means, col="white", main = "Distribution of the Sum of the Avg. of Uniform Distributed Variable")
